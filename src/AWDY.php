e4aed203512fdf772a8b1b456549acc747b7244a
RobertWesner <robert@wesner.io>
2025-10-07 16:46:31 +0200

    Allow AWDY to print to STDERR.

--- src/AWDY.php
+++ src/AWDY.php
@@ -9,6 +9,7 @@ use RobertWesner\AWDY\Template\TemplateInterface;
 
 final class AWDY
 {
+    private static mixed $outputHandle;
     private static ?int $fixedWidth = null;
     private static ?int $fixedHeight = null;
     private static int $previousWidth = 0;
@@ -16,47 +17,19 @@ final class AWDY
 
     private static TemplateInterface $template;
 
-    private static function getWidth(): int
-    {
-        return self::$fixedWidth ?? (int)exec('tput cols');
-    }
-
-    private static function getHeight(): int
-    {
-        return self::$fixedHeight ?? (int)exec('tput lines');
-    }
-
-    private static function render(): void
-    {
-        $width = self::getWidth();
-        $height = self::getHeight();
-
-        if ($width !== self::$previousWidth || $height !== self::$previousHeight) {
-            echo self::$template->getBorder()->getBuffer(self::getWidth(), self::getHeight());
-            echo AnsiEscape::moveToBeginning();
-
-            self::$previousWidth = $width;
-            self::$previousHeight = $height;
-
-            // mark all as dirty on resize so everything will be properly rendered again
-            array_map(fn (Area $area) => $area->dirty(), self::$template->getAreas());
-        }
-
-        foreach (self::$template->getAreas() as $area) {
-            $area->render($width, $height);
-            echo AnsiEscape::resetColor();
-            echo AnsiEscape::moveToBeginning();
-        }
-    }
-
-    public static function setUp(TemplateInterface $template, ?int $width = null, ?int $height = null): void
-    {
+    public static function setUp(
+        TemplateInterface $template,
+        ?int $width = null,
+        ?int $height = null,
+        mixed $output = null
+    ): void {
         self::$template = $template;
         self::$fixedWidth = $width;
         self::$fixedHeight = $height;
+        self::$outputHandle = $output;
 
-        echo AnsiEscape::clear();
-        echo AnsiEscape::moveToBeginning();
+        self::__out(AnsiEscape::clear());
+        self::__out(AnsiEscape::moveToBeginning());
 
         self::render();
     }
@@ -84,11 +57,54 @@ final class AWDY
         self::render();
     }
 
+    public static function __out(string $text): void
+    {
+        if (self::$outputHandle === null) {
+            // deliberately not using STDOUT as default, since it does not work with ob_*()
+            echo $text;
+        } else {
+            fwrite(self::$outputHandle, $text);
+        }
+    }
+
     /**
      * If you so wish as to clear the screen after being done.
      */
     public static function clear(): void
     {
-        echo AnsiEscape::clear();
+        self::__out(AnsiEscape::clear());
+    }
+
+    private static function getWidth(): int
+    {
+        return self::$fixedWidth ?? (int)exec('tput cols');
+    }
+
+    private static function getHeight(): int
+    {
+        return self::$fixedHeight ?? (int)exec('tput lines');
+    }
+
+    private static function render(): void
+    {
+        $width = self::getWidth();
+        $height = self::getHeight();
+
+        if ($width !== self::$previousWidth || $height !== self::$previousHeight) {
+            self::__out((string)self::$template->getBorder()->getBuffer(self::getWidth(), self::getHeight()));
+            self::__out(AnsiEscape::moveToBeginning());
+
+            self::$previousWidth = $width;
+            self::$previousHeight = $height;
+
+            // mark all as dirty on resize so everything will be properly rendered again
+            array_map(fn (Area $area) => $area->dirty(), self::$template->getAreas());
+        }
+
+        foreach (self::$template->getAreas() as $area) {
+            $area->render($width, $height);
+            self::__out(AnsiEscape::resetColor());
+            self::__out(AnsiEscape::moveToBeginning());
+        }
     }
 }
